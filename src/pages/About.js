134bc8bfc781e0876bc69b7fb0c9c36a32179cca
AK-1234-ai <attakhan247@gmail.com>
2020-08-31 19:42:41 +0500

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi
+        distinctio quos, ad accusantium deleniti magnam doloribus repellendus!
+        Quaerat magnam, tempore sint laborum, dolores recusandae commodi
+        reprehenderit velit nemo labore incidunt.
+      </p>
+    </section>
+  );
+}
